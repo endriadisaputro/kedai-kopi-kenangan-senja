7d002cbe6b0c71b3f4befea85037ee890e9fe1fb
endriadisaputro <endryady77@gmail.com>
2023-11-26 16:32:11 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,63 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const shoppingCart = document.querySelector(".shopping-cart");
+
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+// Ketika Search di klik
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventdefault();
+};
+// Ketika Tombol Shopping cart di klik
+document.querySelector("#shopping-cart-button").onclick = () => {
+  shoppingCart.classList.toggle("active");
+};
 
 // klik diluar navbar untuk menghilangkan kelas aktiv
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
+const cartButton = document.querySelector("#shopping-cart-button");
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  // Klik diluar search
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  // Klik diluar Shopping Cart
+  if (!cartButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Detail Produk
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventdefault();
+  };
+});
+
+// Ketika tombol close diklik
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventdefault();
+};
+
+// Ketika Klik diluar Modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
